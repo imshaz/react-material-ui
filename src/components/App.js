0d282f6d3763afd7a5236557220dfa6557023083
imshaz <imshazbangash@gmail.com>
2020-04-30 09:04:18 -0400

    material-ui setup

---
+++ src/components/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+import Button from "@material-ui/core/Button";
+
+function App() {
+  return (
+    <div className="App">
+      <Button variant="contained">Default</Button>
+    </div>
+  );
+}
+
+export default App;
